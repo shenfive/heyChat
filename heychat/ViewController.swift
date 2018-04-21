1582a6c2370a96201fa5c24e57da0ae3d260e295
shenfive @ Mac <shenfive@gmail.com>
2018-04-21 22:37:44 +0800

    Initial Commit

---
+++ heychat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  heychat
+//
+//  Created by 申潤五 on 2018/4/21.
+//  Copyright © 2018年 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
